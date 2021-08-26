7851d1e4f42cfe0fa1f01b52340573e392ab3c6e
Jose <davidherrera0429@gmail.com>
2021-08-26 16:38:45 -0500

    other grat commit

--- js/products.js
+++ js/products.js
@@ -1,29 +1,67 @@
-let closeModal = document.querySelectorAll(".close-modal")[0];
-let openModal = document.querySelectorAll(".product-link")[0];
-let modal = document.querySelectorAll(".modal-products")[0];
-let modalContainer = document.querySelectorAll(".modal-container")[0];
+const accessories = [{
+    name: "Chulo tipo pañoleta",
+    price: "$12.000 Cop",
+    materials: "Saten - microfibra",
+    category: "Accesorio",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores quidem dolore asperiores nemo saepe in, aliquid perspiciatis error vitae debitis consequatur cupiditate aspernatur cum! Quae quam mollitia consequuntur molestiae atque!",
+    img1: "./img/products/gorros1.JPG",
+    img2: "./img/products/gorros1.JPG",
+    img3: "./img/products/gorros1.JPG",
+    img4: "./img/products/gorros1.JPG"
+}];
 
-openModal.addEventListener("click", function(e){
-    e.preventDefault();
-    modalContainer.style.opacity = "1";
-    modalContainer.style.visibility = "visible";
-    modal.classList.toggle("modal-show-hiden"); //remove class "modal-show", if use this method again, the class "modal-show" it adds
+const shower = [{
+    name: "Toalla tipo turbante",
+    price: "$25.000 Cop",
+    materials: "Microfibra",
+    category: "Ducha",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores quidem dolore asperiores nemo saepe in, aliquid perspiciatis error vitae debitis consequatur cupiditate aspernatur cum! Quae quam mollitia consequuntur molestiae atque!",
+    img1: "./img/products/gorros3.JPG",
+    img2: "./img/products/gorros3.JPG",
+    img3: "./img/products/gorros3.JPG",
+    img4: "./img/products/gorros3.JPG"
+}];
+
+const sleep = [{
+    name: "Gorro encarterar",
+    price: "$25.000 Cop",
+    materials: "Satén",
+    category: "Para dormir",
+    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores quidem dolore asperiores nemo saepe in, aliquid perspiciatis error vitae debitis consequatur cupiditate aspernatur cum! Quae quam mollitia consequuntur molestiae atque!",
+    img1: "./img/products/gorros-azules.jpg",
+    img2: "./img/products/gorros-azules.jpg",
+    img3: "./img/products/gorros-azules.jpg",
+    img4: "./img/products/gorros-azules.jpg"
+}];
+
+let btnAccessories = document.querySelector("#accessories");
+let btnShower = document.querySelector("#shower");
+let btnSleep = document.querySelector("#sleep");
+
+const mainContainer = document.getElementById("main-container");
+
+btnAccessories.addEventListener("click", function(e){
+    showAccessories();
 });
 
-function hiddenModal(){
-    modal.classList.toggle("modal-show-hiden");
+btnShower.addEventListener("click", function(e){
+    showShower();
+});
 
-    modalContainer.style.opacity = "0";
-    modalContainer.style.visibility = "hidden";
+btnSleep.addEventListener("click", function(e){
+    showSleep();
+});
+
+function showAccessories (){
+    console.log(accessories);
 }
 
-closeModal.addEventListener("click", function(e){
-    hiddenModal();
-});
+function showShower (){
+    console.log(shower);
+}
 
-window.addEventListener("click", function (e){ //function to close modal in case clik on "modal-container"
-    if (e.target == modalContainer) { //case clik on "modal-container"
-        hiddenModal();
-        console.log("necesito que te cierres");
-    }
-});
+function showSleep(){
+    console.log(sleep);
+}
+
+
