c27212f543e400f3b5613561babc227ae205609e
Jose <davidherrera0429@gmail.com>
2021-09-03 01:48:01 -0500

    products section advanced

--- js/modal-products.js
+++ js/modal-products.js
@@ -1,13 +1,15 @@
 let closeModal = document.querySelectorAll(".close-modal")[0];
-let openModal = document.querySelectorAll(".product-link")[0];
+let productLink = document.querySelectorAll(".product-link")[0];
 let modal = document.querySelectorAll(".modal-products")[0];
 let modalContainer = document.querySelectorAll(".modal-container")[0];
 
-openModal.addEventListener("click", function(e){
-    e.preventDefault();
-    modalContainer.style.opacity = "1";
-    modalContainer.style.visibility = "visible";
-    modal.classList.toggle("modal-show-hiden"); //remove class "modal-show", if use this method again, the class "modal-show" it adds
+productLink.addEventListener("click", function(e){
+    // e.preventDefault();
+    openModal();
+   
+});
+closeModal.addEventListener("click", function(e){
+    hiddenModal();
 });
 
 function hiddenModal(){
@@ -17,13 +19,16 @@ function hiddenModal(){
     modalContainer.style.visibility = "hidden";
 }
 
-closeModal.addEventListener("click", function(e){
-    hiddenModal();
-});
+function openModal () {
+    modalContainer.style.opacity = "1";
+    modalContainer.style.visibility = "visible";
+    modal.classList.toggle("modal-show-hiden"); //remove class "modal-show", if use this method again, the class "modal-show" it adds
+}
+
 
 window.addEventListener("click", function (e){ //function to close modal in case clik on "modal-container"
+    
     if (e.target == modalContainer) { //case clik on "modal-container"
         hiddenModal();
-        console.log("necesito que te cierres");
     }
 });
--- js/products.js
+++ js/products.js
@@ -1,67 +1,156 @@
-const accessories = [{
-    name: "Chulo tipo pañoleta",
-    price: "$12.000 Cop",
-    materials: "Saten - microfibra",
-    category: "Accesorio",
-    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores quidem dolore asperiores nemo saepe in, aliquid perspiciatis error vitae debitis consequatur cupiditate aspernatur cum! Quae quam mollitia consequuntur molestiae atque!",
-    img1: "./img/products/gorros1.JPG",
-    img2: "./img/products/gorros1.JPG",
-    img3: "./img/products/gorros1.JPG",
-    img4: "./img/products/gorros1.JPG"
-}];
+const products = [
+    {
+        name: "Accesorio",
+        price: "$12.000 Cop",
+        materials: "Saten - microfibra",
+        category: "Accesorio",
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores quidem dolore asperiores nemo saepe in, aliquid perspiciatis error vitae debitis consequatur cupiditate aspernatur cum! Quae quam mollitia consequuntur molestiae atque!",
+        img1: "./img/products/gorros1.JPG",
+        img2: "./img/products/gorros1.JPG",
+        img3: "./img/products/gorros1.JPG",
+        img4: "./img/products/gorros1.JPG"
+    },
+    {
+        name: "Accesorio",
+        price: "$12.000 Cop",
+        materials: "Saten - microfibra",
+        category: "Accesorio",
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores quidem dolore asperiores nemo saepe in, aliquid perspiciatis error vitae debitis consequatur cupiditate aspernatur cum! Quae quam mollitia consequuntur molestiae atque!",
+        img1: "./img/products/gorros1.JPG",
+        img2: "./img/products/gorros1.JPG",
+        img3: "./img/products/gorros1.JPG",
+        img4: "./img/products/gorros1.JPG"
+    },
+    {
+        name: "Accesorio",
+        price: "$50.000 Cop",
+        materials: "Saten - microfibra",
+        category: "Accesorio",
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores quidem dolore asperiores nemo saepe in, aliquid perspiciatis error vitae debitis consequatur cupiditate aspernatur cum! Quae quam mollitia consequuntur molestiae atque!",
+        img1: "./img/products/gorros1.JPG",
+        img2: "./img/products/gorros1.JPG",
+        img3: "./img/products/gorros1.JPG",
+        img4: "./img/products/gorros1.JPG"
+    },
+    {
+        name: "Ducha",
+        price: "$25.000 Cop",
+        materials: "Microfibra",
+        category: "Ducha",
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores quidem dolore asperiores nemo saepe in, aliquid perspiciatis error vitae debitis consequatur cupiditate aspernatur cum! Quae quam mollitia consequuntur molestiae atque!",
+        img1: "./img/products/gorros3.JPG",
+        img2: "./img/products/gorros3.JPG",
+        img3: "./img/products/gorros3.JPG",
+        img4: "./img/products/gorros3.JPG"
+    },
+    {
+        name: "Para dormir",
+        price: "$25.000 Cop",
+        materials: "Satén",
+        category: "Para dormir",
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores quidem dolore asperiores nemo saepe in, aliquid perspiciatis error vitae debitis consequatur cupiditate aspernatur cum! Quae quam mollitia consequuntur molestiae atque!",
+        img1: "./img/products/gorros-azules.jpg",
+        img2: "./img/products/gorros-azules.jpg",
+        img3: "./img/products/gorros-azules.jpg",
+        img4: "./img/products/gorros-azules.jpg"
+    }
 
-const shower = [{
-    name: "Toalla tipo turbante",
-    price: "$25.000 Cop",
-    materials: "Microfibra",
-    category: "Ducha",
-    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores quidem dolore asperiores nemo saepe in, aliquid perspiciatis error vitae debitis consequatur cupiditate aspernatur cum! Quae quam mollitia consequuntur molestiae atque!",
-    img1: "./img/products/gorros3.JPG",
-    img2: "./img/products/gorros3.JPG",
-    img3: "./img/products/gorros3.JPG",
-    img4: "./img/products/gorros3.JPG"
-}];
+];
 
-const sleep = [{
-    name: "Gorro encarterar",
-    price: "$25.000 Cop",
-    materials: "Satén",
-    category: "Para dormir",
-    description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores quidem dolore asperiores nemo saepe in, aliquid perspiciatis error vitae debitis consequatur cupiditate aspernatur cum! Quae quam mollitia consequuntur molestiae atque!",
-    img1: "./img/products/gorros-azules.jpg",
-    img2: "./img/products/gorros-azules.jpg",
-    img3: "./img/products/gorros-azules.jpg",
-    img4: "./img/products/gorros-azules.jpg"
-}];
 
-let btnAccessories = document.querySelector("#accessories");
-let btnShower = document.querySelector("#shower");
-let btnSleep = document.querySelector("#sleep");
+let btnAccessories = document.getElementById("accessories");
+let btnShower = document.getElementById("shower");
+let btnSleep = document.getElementById("sleep");
 
-const mainContainer = document.getElementById("main-container");
+btnAccessories.addEventListener("click", function (e) {
+    showProducts(products,"Accesorio");
+    btnAccessories.style.backgroundColor = "#b78372";
+    btnAccessories.style.border = " 1px solid #b78372";
 
-btnAccessories.addEventListener("click", function(e){
-    showAccessories();
+    btnShower.style = "inherit";
+    btnSleep.style = "inherit";
 });
 
-btnShower.addEventListener("click", function(e){
-    showShower();
+btnShower.addEventListener("click", function (e) {
+    showProducts(products,"Ducha");
+    btnShower.style.backgroundColor = "#b78372";
+    btnShower.style.border = " 1px solid #b78372";
+
+    btnAccessories.style = "inherit";
+    btnSleep.style = "inherit";
 });
 
-btnSleep.addEventListener("click", function(e){
-    showSleep();
+btnSleep.addEventListener("click", function (e) {
+    showProducts(products,"Para dormir");
+    btnSleep.style.backgroundColor = "#b78372";   
+    btnSleep.style.border = " 1px solid #b78372";
+
+    btnAccessories.style = "inherit";
+    btnShower.style = "inherit";
 });
 
-function showAccessories (){
-    console.log(accessories);
+
+let template = " ";
+
+const productsContainer = document.querySelector(".products-container");
+
+function showProducts(items,category) {
+    console.log(items);
+
+
+    //Empty template
+    template = " ";
+    
+
+    //create template with the products
+    items.forEach(item => {
+
+        if(item.category === category){
+            template += ` 
+            <div class="item">
+                <a href="#" class="product-link">
+                <img src="./img/products/gorros1.JPG" alt="">
+                <section class="item-header">
+                    <h2>${item.name}</h2>
+                    <h4>${item.price}</h4>
+                </section>
+                <section class="item-footer">
+                    <h3>3 Und Disponibles <i class="fas fa-shopping-cart"></i></h3>
+                </section>
+                </a>
+            </div>
+            `
+        } else {
+            template += ""
+        }
+
+    })
+
+    //show products
+    productsContainer.innerHTML = template;
+
 }
 
-function showShower (){
-    console.log(shower);
+function showAllProducts(){
+    template = " ";
+
+    items.forEach(item => {
+        template += ` 
+        <div class="item">
+            <a href="#" class="product-link">
+            <img src="./img/products/gorros1.JPG" alt="">
+            <section class="item-header">
+                <h2>${item.name}</h2>
+                <h4>${item.price}</h4>
+            </section>
+            <section class="item-footer">
+                <h3>3 Und Disponibles <i class="fas fa-shopping-cart"></i></h3>
+            </section>
+            </a>
+        </div>
+        `
+    })
 }
 
-function showSleep(){
-    console.log(sleep);
-}
 
 
